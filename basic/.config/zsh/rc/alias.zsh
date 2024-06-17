1d312e639ff4663fc8c10226840a80479ad610e3
Alexander Gertsch <gertscha@student.ethz.ch>
2024-06-17 16:52:31 +0200

    Reworked ZSH config

---
+++ basic/.config/zsh/rc/alias.zsh
@@ -0,0 +1,16 @@
+#--------------------------------------------------------------#
+#           Aliases                                            #
+#--------------------------------------------------------------#
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes.
+# For a full list of active aliases, run `alias`.
+alias ls='ls -a --color=auto'
+alias ll='ls -lah --color=auto'
+alias grep='grep --color=auto'
+alias cl='clear'
+alias vim='nvim'
+
+alias zshec="$EDITOR $ZDOTDIR/.zshrc" # edit .zshrc
+alias zshsc="source $ZDOTDIR/.zshrc"  # reload zsh configuration
+
---
+++ basic/.zshenv
@@ -0,0 +1,8 @@
+# setup basic environment for zsh
+
+# where to look for .zshrc
+export ZDOTDIR=$HOME/.config/zsh
+
+# oh-my-zsh installation
+export ZSH="$HOME/build/oh-my-zsh"
+
