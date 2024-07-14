fbf4f8f58e9f677f617d2e65c91ae7f85739e458
Alexander Gertsch <agertsch11@gmail.com>
2024-07-14 22:54:04 +0200

    Regress which-key version to v2, v3 has breaking changes and the transition is poorly documented

--- neovim/.config/nvim/lua/user/which-key.lua
+++ neovim/.config/nvim/lua/user/which-key.lua
@@ -1,6 +1,7 @@
 local M = {
   'folke/which-key.nvim',
   event = 'VeryLazy',
+  tag = 'v2.1.0',
   -- make sure timeoutlen may interfere, check doc if not working
   opts = {
     -- your configuration comes here
