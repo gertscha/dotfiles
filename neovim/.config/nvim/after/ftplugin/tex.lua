0ab1c1d5d0073fe03aa65cb41adade1dc720d092
Alexander Gertsch <agertsch11@gmail.com>
2024-07-30 23:17:07 +0200

    Update nvim: set tab size to 2 for .tex

--- neovim/.config/nvim/after/ftplugin/tex.lua
+++ neovim/.config/nvim/after/ftplugin/tex.lua
@@ -1,5 +1,7 @@
 -- disable autoindent for tex files, since it does not work properly
 vim.opt_local.autoindent = false
 vim.opt_local.smartindent = false
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
 
 vim.cmd('setlocal spell')
