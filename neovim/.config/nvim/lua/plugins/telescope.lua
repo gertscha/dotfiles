eaac270a365ff584d9cf196e7971962398f01bac
Alexander Gertsch <agertsch11@gmail.com>
2025-01-06 17:57:31 +0100

    nvim: Update Telescope version and added fzf-native for its search

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -4,110 +4,112 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = 'TelescopeResults',
   callback = function(ctx)
     vim.api.nvim_buf_call(ctx.buf, function()
-    vim.fn.matchadd('TelescopeParent', '\t\t.*$')
-    vim.api.nvim_set_hl(0, 'TelescopeParent', { link = 'Comment' })
-  end)
-end,
+      vim.fn.matchadd('TelescopeParent', '\t\t.*$')
+      vim.api.nvim_set_hl(0, 'TelescopeParent', { link = 'Comment' })
+    end)
+  end,
 })
 -- show the file name first and the path at the end
 local function filenameFirst(_, path)
   local tail = vim.fs.basename(path)
   local parent = vim.fs.dirname(path)
   if parent == '.' then return tail end
-  return string.format('%s\t\t%s', tail,parent)
+  return string.format('%s\t\t%s', tail, parent)
 end
 
 
 -- fuzzy finder
 local M = {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.5',
-  cmd = 'Telescope',
-  keys = {
-    { '<leader>s', desc='Telescope' },
-    { '<leader>sf', desc='[S]earch [F]iles' },
-    { '<leader>sb', desc='[S]earch [B]uffers' },
-    { '<leader>sr', desc='[S]earch Git [R]epository' },
-    { '<leader>sh', desc='[S]earch [H]elp' },
-    { '<leader>sw', desc='[S]earch current [W]ord' },
-    { '<leader>sg', desc='[S]earch by [G]rep' },
-  },
+  branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
-  opts = function()
-    local icons = require('settings.icons')
-    local actions = require('telescope.actions')
-    require('telescope').setup {
-      defaults = {
-        prompt_prefix = icons.ui.Telescope .. ' ',
-        selection_caret = icons.ui.Forward .. ' ',
-        entry_prefix = '   ',
-        initial_mode = 'insert',
-        selection_strategy = 'reset',
-        path_display = { 'smart' },
-        color_devicons = true,
-        mappings = {
-          n = {
-            ['<esc>'] = actions.close,
-            ['j'] = actions.move_selection_next,
-            ['k'] = actions.move_selection_previous,
-            ['q'] = actions.close,
-          },
-          i = {
-            ['<esc>'] = actions.close,
-          }
-        },
-      },
-      pickers = {
-        live_grep = {
-          additional_args = function()
-            return { '--hidden' }
-          end,
-        },
-        find_files = {
-          theme = 'dropdown',
-          previewer = false,
-          path_display = filenameFirst,
-        },
-        buffers = {
-          theme = 'dropdown',
-          previewer = false,
-          path_display = filenameFirst,
-        },
-      },
-    }
-  end,
+  cmd = 'Telescope',
+  keys = { '<leader>s', desc = 'Telescope' },
 }
 
 function M.config()
-  -- which-key setup this is the default
-  local kopts = {
-    mode = 'n', -- NORMAL mode
-    prefix = '', -- the prefix is prepended to every mapping part of `mappings`
-    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true, -- use `silent` when creating keymaps
-    noremap = true, -- use `noremap` when creating keymaps
-    nowait = false, -- use `nowait` when creating keymaps
-    expr = false, -- use `expr` when creating keymaps
-  }
-
-  require('which-key').register({
-    ['<leader>'] = {
-      s = {
-        name = 'Telescope',
-        f = { '<cmd>Telescope find_files<cr>', '[S]earch [F]iles' },
-        b = { '<cmd>Telescope buffers<cr>', '[S]earch [B]uffers' },
-        r = { '<cmd>Telescope git_files<cr>', '[S]earch Git [R]epository' },
-        h = { '<cmd>Telescope help_tags<cr>', '[S]earch [H]elp' },
-        w = { '<cmd>Telescope grep_string<cr>', '[S]earch current [W]ord' },
-        g = { '<cmd>Telescope live_grep<cr>', '[S]earch by [G]rep' },
+  -- which-key setup (make sure it matches the load keys)
+  require('which-key').add({
+    -- make sure this matches the keys entry for the plugin loading
+    { '<leader>s', group = 'Telescope' },
+    {
+      mode = 'n',
+      silent = true,  -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      { '<leader>sf', '<cmd>Telescope find_files<cr>',     desc = '[S]earch [F]iles' },
+      { '<leader>sr', '<cmd>Telescope git_files<cr>',      desc = '[S]earch Git [R]epository' },
+      { '<leader>sh', '<cmd>Telescope help_tags<cr>',      desc = '[S]earch [H]elp' },
+      { '<leader>sw', '<cmd>Telescope grep_string<cr>',    desc = '[S]earch current [W]ord' },
+      { '<leader>sg', '<cmd>Telescope live_grep<cr>',      desc = '[S]earch by [G]rep' },
+      { '<leader>sj', '<cmd>Telescope jumplist<cr>',       desc = '[S]earch [J]umplist' },
+      { '<leader>ss', '<cmd>Telescope search_history<cr>', desc = '[S]earch [S]earch history' },
+      { '<leader>sm', '<cmd>Telescope marks<cr>',          desc = '[S]earch [M]arks' },
+      {
+        '<leader>sc',
+        function()
+          require('telescope.builtin').find_files { cwd = vim.fn.stdpath('config') }
+        end,
+        desc = '[S]earch [C]onfig files'
+      },
+      {
+        '<leader>sp',
+        function()
+          require('telescope.builtin').find_files {
+            cwd = vim.fs.joinpath(vim.fn.stdpath('data'), 'lazy')
+          }
+        end,
+        desc = '[S]earch [P]lugin implementations'
       },
     },
-  }, kopts)
+  })
 
+  local icons = require('settings.icons')
+  local actions = require('telescope.actions')
+  require('telescope').setup {
+    defaults = {
+      prompt_prefix = icons.ui.Telescope,
+      selection_caret = icons.ui.Forward,
+      entry_prefix = '  ',
+      initial_mode = 'insert',
+      path_display = { 'smart' },
+      color_devicons = true,
+      mappings = {
+        n = {
+          ['<esc>'] = actions.close,
+          ['j'] = actions.move_selection_next,
+          ['k'] = actions.move_selection_previous,
+          ['q'] = actions.close,
+        },
+      },
+    },
+    extensions = {
+      fzf = {},
+    },
+    pickers = {
+      live_grep = {
+        additional_args = function()
+          return { '--hidden' }
+        end,
+      },
+      find_files = {
+        theme = 'dropdown',
+        previewer = false,
+        path_display = filenameFirst,
+      },
+      buffers = {
+        theme = 'dropdown',
+        previewer = false,
+        path_display = filenameFirst,
+      },
+    },
+  }
+
+  -- To get fzf loaded and working with telescope, you need to call
+  -- load_extension, somewhere after setup function:
+  require('telescope').load_extension('fzf')
 end
 
 return M
-
