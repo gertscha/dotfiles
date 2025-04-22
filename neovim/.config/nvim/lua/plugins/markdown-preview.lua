25604d1c1f17bb0bd4b029afad9d92930adb1ae5
Alexander Gertsch <agertsch11@gmail.com>
2025-04-22 20:29:57 +0200

    fix(neovim): resolve buggy startup of Markdown-Preview, caused by lazy loading

--- neovim/.config/nvim/lua/plugins/markdown-preview.lua
+++ neovim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,17 +1,29 @@
 local M = {
   "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   build = function()
     require("lazy").load({ plugins = { "markdown-preview.nvim" } })
     vim.fn["mkdp#util#install"]()
   end,
-  keys = {
-    {
-      "<leader>omp",
-      "<cmd>MarkdownPreviewToggle<cr>",
-      desc = "Markdown Preview",
-    },
-  },
+  event = 'VeryLazy',
+  -- cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  -- keys = {
+  --   {
+  --     "<leader>omp",
+  --     "<cmd>MarkdownPreviewToggle<cr>",
+  --     desc = "Markdown Preview",
+  --   },
+  -- },
 }
+
+function M.config()
+  require('which-key').add({
+    mode = 'n',     -- NORMAL mode
+    buffer = nil,   -- nil for Global mappings. Give buffer number for buffer local mappings
+    silent = true,  -- use `silent` when creating keymaps
+    noremap = true, -- use `noremap` when creating keymaps
+    nowait = false, -- use `nowait` when creating keymaps
+    { '<leader>omp', '<cmd>MarkdownPreviewToggle<cr>', desc = 'Markdown Preview' },
+  })
+end
 
 return M
