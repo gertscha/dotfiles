cbf2d055539132f5e81df83e8cfa2bf5746dc816
Alexander Gertsch <agertsch11@gmail.com>
2025-04-22 21:00:58 +0200

    fix(fish): correct git status icon to centered circle

--- basic/.config/fish/functions/fish_right_prompt.fish
+++ basic/.config/fish/functions/fish_right_prompt.fish
@@ -21,7 +21,7 @@ function fish_right_prompt
     set -g __fish_git_prompt_char_upstream_ahead "↑"
     set -g __fish_git_prompt_char_upstream_behind "↓"
     set -g __fish_git_prompt_char_upstream_prefix ""
-    set -g __fish_git_prompt_char_stagedstate "●"
+    set -g __fish_git_prompt_char_stagedstate ""
     set -g __fish_git_prompt_char_dirtystate "+"
     set -g __fish_git_prompt_char_untrackedfiles "…"
     set -g __fish_git_prompt_char_conflictedstate "✖"
