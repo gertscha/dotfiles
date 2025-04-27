62b04b61d976c3ad8b6a455904a8a0a88d84983e
Alexander Gertsch <agertsch11@gmail.com>
2025-04-27 22:30:41 +0200

    feat(fish): configure ls and grep colors

--- basic/.config/fish/config.fish
+++ basic/.config/fish/config.fish
@@ -2,4 +2,6 @@ if status is-interactive
     set -gx EDITOR nvim
 end
 
-
+set -gx LS_COLORS "di=32:ln=36:so=1;37:pi=1;37:ex=1;31:bd=37:cd=37:su=30;47:sg=30;47:tw=1;32:ow=1;32"
+set -gx LSCOLORS "cxgxHxHxBxhxhxahahCxCx"
+set -gx GREP_COLORS "sl=49;39:cx=49;39:mt=49;31;1:fn=49;32:ln=49;33:bn=49;33:se=1;36"
