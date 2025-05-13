3626f116398707c41a95211c3a0f0bc8cd716baa
Alexander Gertsch <gertscha@student.ethz.ch>
2025-05-13 09:21:34 +0200

    feat(neovim): add more LaTex Snippets

--- neovim/.config/nvim/lua/snippets/tex.lua
+++ neovim/.config/nvim/lua/snippets/tex.lua
@@ -30,4 +30,13 @@ return {
         rep(1),
       })
   ),
+  s({ trig = "tt", snippetType = "snippet", wordTrig = false },
+    fmta([[\texttt{<>}]], {i(1)})
+  ),
+  s({ trig = "bf", snippetType = "snippet", wordTrig = false },
+    fmta([[\textbf{<>}]], {i(1)})
+  ),
+  s({ trig = "it", snippetType = "snippet", wordTrig = false },
+    fmta([[\textit{<>}]], {i(1)})
+  ),
 }
