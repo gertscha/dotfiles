634c227f19243c203212d767ea817a48ce1bea45
Alexander Gertsch <agertsch11@gmail.com>
2025-05-25 21:22:01 +0200

    feat(neovim): add more snippets for C/C++

--- neovim/.config/nvim/lua/snippets/c.lua
+++ neovim/.config/nvim/lua/snippets/c.lua
@@ -25,10 +25,7 @@ end
 return {
   s(
     { trig = 'if', snippetType = 'snippet', wordTrig = false },
-    fmta(
-      [[if (<>) {]] .. '\n' .. [[<><>]] .. '\n' .. [[}]],
-      { i(1), t('\t'), i(2) }
-    )
+    fmta([[if (<>) {]] .. '\n' .. [[<><>]] .. '\n' .. [[}]], { i(1), t('\t'), i(2) })
   ),
   s(
     { trig = 'for', snippetType = 'snippet', wordTrig = false },
@@ -36,11 +33,10 @@ return {
       [[for (<> <> = 0; <> << <>; <>++) {]] .. '\n' .. [[<><>]] .. '\n' .. [[}]],
       {
         c(1, {
-          t('[type]'),
-          t('unsigned int'),
-          t('int'),
-          t('size_t'),
           i(nil, '...'),
+          t('unsigned int'),
+          t('size_t'),
+          t('int'),
         }),
         i(2),
         rep(2),
@@ -52,7 +48,36 @@ return {
     )
   ),
   s(
-    { trig = 'header' },
+    { trig = 'func', snippetType = 'snippet', wordTrig = false },
+    fmta([[<> <>(<>) {]] .. '\n' .. [[<><>]] .. '\n' .. [[}]], {
+      c(1, { i(nil, '...'), t('void'), t('int'), t('double') }),
+      i(2),
+      i(3),
+      t('\t'),
+      i(4),
+    })
+  ),
+  s(
+    { trig = 'switch', snippetType = 'snippet', wordTrig = false },
+    fmta(
+      [[
+      switch (<>) {
+      <>case <>:
+      <><>
+      <>break;
+      <>default:
+      <><>
+      }
+      ]],
+      { i(1), t('\t'), i(2), t('\t\t'), i(3), t('\t\t'), t('\t'), t('\t\t'), i(4) }
+    )
+  ),
+  s(
+    { trig = '#', snippetType = 'snippet', wordTrig = false },
+    fmta([[#include <<<>>>]], { i(1) })
+  ),
+  s(
+    { trig = 'header', snippetType = 'snippet', wordTrig = false },
     fmta(
       [[
     #ifndef <guard>
