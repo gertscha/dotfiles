d5fb32764f749e08a619ac2a1a4173129dd7ab4b
Alexander Gertsch <agertsch11@gmail.com>
2025-07-10 16:41:41 +0200

    feat(neovim): add word toggle plugin

---
+++ neovim/.config/nvim/lua/plugins/toggler.lua
@@ -0,0 +1,25 @@
+local M = {
+  'nvim-toggler', -- fork of: 'nguyenvukhang/nvim-toggler'
+  dir = vim.fn.stdpath('config') .. '/custom/nvim-toggler',
+  event = { 'BufRead', 'BufNewFile' },
+  opts = {
+    inverses = {
+      ['true'] = 'false',
+      ['True'] = 'False',
+      ['TRUE'] = 'FALSE',
+      ['yes'] = 'no',
+      ['on'] = 'off',
+      ['left'] = 'right',
+      ['up'] = 'down',
+      ['enable'] = 'disable',
+      ['!='] = '==',
+      ['<'] = '>=',
+      ['>'] = '<=',
+    },
+    remove_default_keybinds = false,
+    remove_default_inverses = false,
+    autoselect_longest_match = true,
+  },
+}
+
+return M
