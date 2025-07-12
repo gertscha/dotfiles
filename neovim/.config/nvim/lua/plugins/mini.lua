d28a0a92042b049a7537c6e7cdfe1dfbade3dc31
Alexander Gertsch <agertsch11@gmail.com>
2025-07-12 22:42:48 +0200

    feat(neovim): add mini.surround

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -2,17 +2,46 @@ return {
   {
     'echasnovski/mini.icons',
     version = '*',
-    config = function()
-      require('mini.icons').setup()
-    end,
+    config = true,
   },
   {
     -- Extend and create a/i textobjects
     'echasnovski/mini.ai',
     version = '*',
-    event = 'VeryLazy',
-    config = function()
-      require('mini.ai').setup()
-    end,
+    event = { 'BufRead', 'BufNewFile' },
+    opts = {
+      mappings = {
+        around = 'a',
+        inside = 'i',
+        -- NOTE: These override built-in LSP selection mappings on Neovim>=0.12
+        -- Map LSP selection manually to use it (see `:h MiniAi.config`)
+        around_next = 'an',
+        inside_next = 'in',
+        around_last = 'al',
+        inside_last = 'il',
+        -- Move cursor to corresponding edge of `a` textobject
+        goto_left = 'g[',
+        goto_right = 'g]',
+      },
+    },
+  },
+  {
+    -- modify surrounding objects like quotes
+    'echasnovski/mini.surround',
+    version = '*',
+    event = { 'BufRead', 'BufNewFile' },
+    opts = {
+      mappings = {
+        add = 'sa', -- Add surrounding in Normal and Visual modes
+        delete = 'sd', -- Delete surrounding
+        find = 'sf', -- Find surrounding (to the right)
+        find_left = 'sF', -- Find surrounding (to the left)
+        highlight = 'sh', -- Highlight surrounding
+        replace = 'sr', -- Replace surrounding
+        update_n_lines = 'sn', -- Update `n_lines`
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
+      },
+    },
   },
 }
