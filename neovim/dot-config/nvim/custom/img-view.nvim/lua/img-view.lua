cc6291804b3e2aaf589c1590296926617c81f323
Alexander Gertsch <gertscha@student.ethz.ch>
2025-08-02 22:48:19 +0200

    feat(neovim): add custom chafa based image preview (concept only)

---
+++ neovim/dot-config/nvim/custom/img-view.nvim/lua/img-view.lua
@@ -0,0 +1,154 @@
+---@class Viewer
+---@field config {value: any, context: any}
+---@field state {value: any, context: any}
+local Viewer = {}
+Viewer.__index = Viewer
+
+function Viewer:_display(row, col, width, height, image)
+  local proc = assert(io.popen('tty'))
+  local tty_name = proc:read()
+  proc:close()
+
+  -- vim.notify(string.format('size: %dx%d', width, height), vim.log.levels.INFO)
+
+  local cmd = string.format('chafa -s %ix%i %s', width, height, image)
+  local output = nil
+  local handle = io.popen(cmd)
+  if handle then
+    output = handle:read('*all') -- Read all output
+    handle:close()
+  else
+    vim.notify('failed to get blob', vim.log.levels.WARN)
+    return
+  end
+  pcall(function()
+    local stdout = assert(io.open(tty_name, 'ab'))
+    -- format is format(row, col)
+    stdout:write(('\x1b[s\x1b[%d;%dH'):format(row, col))
+    stdout:write(output)
+    stdout:write('\x1b[u')
+    stdout:close()
+  end)
+end
+
+function Viewer:show(image)
+  -- cpos: (row, col) tuple
+  local cpos = vim.api.nvim_win_get_cursor(0)
+  -- spos: table {row,col,endcol,curscol}
+  local spos = vim.fn.screenpos(vim.fn.win_getid(), cpos[1] + 1, 0)
+  local width = vim.o.columns - spos.col + 1
+  local height = vim.o.lines - spos.row
+
+  if self.state.au_cmd_id then self:clean() end
+
+  self._display(self, spos.row, spos.col, width, height, image)
+
+  local auid = vim.api.nvim_create_autocmd('CursorMoved', {
+    group = self.state.augroup,
+    callback = function(ev)
+      self:clean()
+    end,
+  })
+
+  self.state.au_cmd_id = auid
+  self.state.showing = true
+end
+
+function Viewer:show_buf()
+  local file = vim.fn.expand('%')
+  self:show(file)
+end
+
+function Viewer:clean()
+  if self.state.showing then
+    vim.cmd('mode') -- clear screen
+    -- https://github.com/neovim/neovim/issues/10279
+    -- :redraw -> update_screen(0)
+    -- :redraw! -> update_screen(NOT_VALID) <- not sufficient
+    -- :mode -> update_screen(CLEAR) <- need this
+    -- <C-L> -> update_screen(CLEAR)
+    self.state.showing = false
+    if self.state.au_cmd_id then
+      vim.api.nvim_del_autocmd(self.state.au_cmd_id)
+      self.state.au_cmd_id = nil
+    end
+  else
+    vim.notify('No image showing', vim.log.levels.INFO)
+  end
+end
+
+function Viewer.new()
+  local newViewer = {
+    config = config or {},
+    state = {
+      showing = false,
+      hooks_setup = false,
+      au_cmd_id = nil,
+      namespace = nil,
+      augroup = nil,
+    },
+  }
+  setmetatable(newViewer, Viewer)
+
+  return newViewer
+end
+
+local the_viewer = Viewer:new()
+
+---@param self Viewer
+---@param partial_config
+---@return Viewer
+function Viewer.setup(self, partial_config)
+  if self ~= the_viewer then
+    partial_config = self
+    self = the_viewer
+  end
+
+  local defaults = {
+    filetypes = {
+      png = true,
+      jpeg = true,
+      jpg = true,
+    },
+  }
+
+  partial_config = partial_config or {}
+  local config = defaults
+  for k, v in pairs(partial_config) do
+    if k == 'filetypes' then
+      config.filetypes = vim.tbl_extend('force', config.filetypes, v)
+    else
+      config[k] = v
+    end
+  end
+  self.config = config
+
+  local filetypes = config.filetypes
+
+  local namespace = vim.api.nvim_create_namespace('Image.Viewer.ns')
+  self.state.namespace = namespace
+  local augroup = vim.api.nvim_create_augroup('Image.Viewer.au', {})
+  self.state.augroup = augroup
+
+  if self.state.hooks_setup == false then
+    -- vim.api.nvim_create_autocmd('Filetype', {
+    vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+      group = augroup,
+      callback = function(ev)
+        if self.state.showing then self:clean() end
+        local ft = vim.fn.expand('%:e')
+        if filetypes[ft] == true then
+          -- vim.notify('matching ft', vim.log.levels.INFO)
+          vim.schedule(function()
+            self:show_buf()
+          end)
+        end
+      end,
+    })
+    self.state.hooks_setup = true
+  end
+
+  return self
+end
+
+return the_viewer
