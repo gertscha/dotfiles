1bfc73840fa819739200a45fe38716bbb25c2f86
Alexander Gertsch <agertsch11@gmail.com>
2025-09-15 10:46:28 +0200

    feat(wezterm): refine config, tab behavior, keybinds

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -26,7 +26,7 @@ local config = {
 	},
 	-- window_background_opacity = background_opacity,
 
-	-------------------------------- Behaviour --------------------------------
+	-------------------------------- Behavior --------------------------------
 	default_prog = { "fish" },
 	default_cwd = "~",
 
@@ -56,7 +56,7 @@ local config = {
 	enable_tab_bar = true,
 	use_fancy_tab_bar = false,
 	-- colors are set in theme
-	hide_tab_bar_if_only_one_tab = true,
+	hide_tab_bar_if_only_one_tab = false,
 
 	----------------------------------- Bell ----------------------------------
 	audible_bell = "Disabled",
@@ -71,7 +71,56 @@ local config = {
 	keys = {
 		-- { key = "R", mods = "CTRL|ALT", action = act.ReloadConfiguration },
 		{ key = "p", mods = "CTRL|ALT", action = wezterm.action.ShowLauncher },
-		{ key = "O", mods = "CTRL|ALT|SHIFT", action = act.ShowDebugOverlay },
+		{ key = "o", mods = "CTRL|ALT|SHIFT", action = act.ShowDebugOverlay },
+		{ key = "x", mods = "CTRL|ALT", action = act.ActivateCopyMode },
+		-- workspaces
+		{ key = "{", mods = "CTRL|ALT", action = act.SwitchWorkspaceRelative(-1) },
+		{ key = "}", mods = "CTRL|ALT", action = act.SwitchWorkspaceRelative(1) },
+		{ key = "s", mods = "CTRL|ALT", action = act.ShowLauncherArgs({ flags = "WORKSPACES" }) },
+		{
+			-- Prompt for a workspace name (create it) and switch to it
+			key = "S",
+			mods = "CTRL|ALT",
+			action = act.PromptInputLine({
+				description = wezterm.format({
+					{ Attribute = { Intensity = "Bold" } },
+					{ Foreground = { AnsiColor = "Fuchsia" } },
+					{ Text = "Enter name for (new) workspace to switch to" },
+				}),
+				action = wezterm.action_callback(function(window, pane, line)
+					-- line will be `nil` if they hit escape without entering anything
+					if line then
+						if line == "" then -- use random name
+							window:perform_action(act.SwitchToWorkspace, pane)
+						else
+							window:perform_action(act.SwitchToWorkspace({ name = line }), pane)
+						end
+					end
+				end),
+			}),
+		},
+		{ key = "a", mods = "CTRL|ALT", action = act.SwitchToWorkspace({ name = "default" }) },
+		{
+			-- Prompt for a new name for the current workspace
+			key = "A",
+			mods = "CTRL|ALT",
+			action = act.PromptInputLine({
+				description = wezterm.format({
+					{ Attribute = { Intensity = "Bold" } },
+					{ Foreground = { AnsiColor = "Fuchsia" } },
+					{ Text = "Rename the current workspace" },
+				}),
+				action = wezterm.action_callback(function(window, pane, line)
+					-- line will be `nil` if they hit escape without entering anything
+					if line then
+						mux.rename_workspace(window:active_workspace(), line)
+					end
+				end),
+			}),
+		},
+		-- workspace persistence
+		-- { key = "d", mods = "CTRL|ALT", action = M.save_workspaces },
+		-- { key = "f", mods = "CTRL|ALT", action = M.load_workspaces },
 		-- window
 		{ key = "n", mods = "CTRL|ALT", action = act.SpawnWindow },
 		{ key = "f", mods = "CTRL|ALT", action = act.ToggleFullScreen },
@@ -87,7 +136,19 @@ local config = {
 		{ key = "End", mods = "SHIFT", action = act.ScrollToBottom },
 		{ key = "I", mods = "CTRL|ALT", action = act.EmitEvent("toggle-opacity") },
 		-- tabs
-		{ key = "t", mods = "CTRL|ALT", action = act.SpawnTab("CurrentPaneDomain") },
+		-- { key = "t", mods = "CTRL|ALT", action = act.SpawnTab("CurrentPaneDomain") },
+		{
+			-- spawn tab and set its name immediately
+			key = "t",
+			mods = "CTRL|ALT",
+			action = act.Multiple({
+				act.SpawnTab("CurrentPaneDomain"),
+				wezterm.action_callback(function(window)
+					window:active_tab():set_title("-")
+				end),
+			}),
+		},
+
 		{ key = "w", mods = "CTRL|ALT", action = act.CloseCurrentTab({ confirm = false }) },
 		{ key = "[", mods = "CTRL|ALT", action = act.ActivateTabRelative(-1) },
 		{ key = "]", mods = "CTRL|ALT", action = act.ActivateTabRelative(1) },
@@ -105,6 +166,19 @@ local config = {
 			mods = "CTRL|ALT",
 			action = act.ShowLauncherArgs({ flags = "TABS" }),
 		},
+		{
+			key = "E",
+			mods = "CTRL|ALT",
+			action = act.PromptInputLine({
+				description = "Enter new name for current tab",
+				action = wezterm.action_callback(function(window, pane, line)
+					-- line will be `nil` if they hit escape without entering anything
+					if line and line ~= "" then
+						window:active_tab():set_title(line)
+					end
+				end),
+			}),
+		},
 		-- panes
 		{ key = '"', mods = "ALT|CTRL", action = act.SplitVertical },
 		{ key = '"', mods = "SHIFT|ALT|CTRL", action = act.SplitVertical },
@@ -120,55 +194,6 @@ local config = {
 		{ key = "H", mods = "ALT|CTRL", action = act.AdjustPaneSize({ "Left", 2 }) },
 		{ key = "K", mods = "ALT|CTRL", action = act.AdjustPaneSize({ "Up", 2 }) },
 		{ key = "J", mods = "ALT|CTRL", action = act.AdjustPaneSize({ "Down", 2 }) },
-		-- workspaces
-		{ key = "{", mods = "CTRL|ALT", action = act.SwitchWorkspaceRelative(-1) },
-		{ key = "}", mods = "CTRL|ALT", action = act.SwitchWorkspaceRelative(1) },
-		-- { key = "d", mods = "CTRL|ALT", action = M.save_workspaces },
-		-- { key = "f", mods = "CTRL|ALT", action = M.load_workspaces },
-		{ key = "s", mods = "CTRL|ALT", action = act.ShowLauncherArgs({ flags = "WORKSPACES" }) },
-		{
-			-- Prompt for a new name for the current workspace
-			key = "S",
-			mods = "CTRL|ALT",
-			action = act.PromptInputLine({
-				description = wezterm.format({
-					{ Attribute = { Intensity = "Bold" } },
-					{ Foreground = { AnsiColor = "Fuchsia" } },
-					{ Text = "Enter new name for current workspace" },
-				}),
-				action = wezterm.action_callback(function(window, pane, line)
-					-- line will be `nil` if they hit escape without entering anything
-					if line then
-						mux.rename_workspace(window:active_workspace(), line)
-					end
-				end),
-			}),
-		},
-		{ key = "a", mods = "CTRL|ALT", action = act.SwitchToWorkspace({ name = "default" }) },
-		{
-			-- Prompt for a workspace name (create it) and switch to it
-			key = "A",
-			mods = "CTRL|ALT",
-			action = act.PromptInputLine({
-				description = wezterm.format({
-					{ Attribute = { Intensity = "Bold" } },
-					{ Foreground = { AnsiColor = "Fuchsia" } },
-					{ Text = "Enter name for (new) workspace" },
-				}),
-				action = wezterm.action_callback(function(window, pane, line)
-					-- line will be `nil` if they hit escape without entering anything
-					if line then
-						if line == "" then -- use random name
-							window:perform_action(act.SwitchToWorkspace, pane)
-						else
-							window:perform_action(act.SwitchToWorkspace({ name = line }), pane)
-						end
-					end
-				end),
-			}),
-		},
-		-- modes
-		{ key = "x", mods = "CTRL|ALT", action = act.ActivateCopyMode },
 	},
 
 	key_tables = {
@@ -177,7 +202,6 @@ local config = {
 			{ key = "c", mods = "CTRL", action = act.Multiple({ "ScrollToBottom", { CopyMode = "Close" } }) },
 			{ key = "q", mods = "NONE", action = act.Multiple({ "ScrollToBottom", { CopyMode = "Close" } }) },
 			{ key = "Enter", mods = "NONE", action = act.CopyMode("MoveToStartOfNextLine") },
-			{ key = "V", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Line" }) },
 			{ key = "$", mods = "NONE", action = act.CopyMode("MoveToEndOfLineContent") },
 			{ key = "0", mods = "NONE", action = act.CopyMode("MoveToStartOfLine") },
 			{ key = "^", mods = "NONE", action = act.CopyMode("MoveToStartOfLineContent") },
@@ -203,6 +227,7 @@ local config = {
 			{ key = "o", mods = "NONE", action = act.CopyMode("MoveToSelectionOtherEnd") },
 			{ key = "t", mods = "NONE", action = act.CopyMode({ JumpForward = { prev_char = true } }) },
 			{ key = "v", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Cell" }) },
+			{ key = "V", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Line" }) },
 			{ key = "v", mods = "CTRL", action = act.CopyMode({ SetSelectionMode = "Block" }) },
 			{
 				key = "y",
@@ -232,7 +257,7 @@ local config = {
 -- 	window:toast_notification("wezterm", "configuration reloaded!", nil, 4000)
 -- end)
 
-wezterm.on("toggle-opacity", function(window, pane)
+wezterm.on("toggle-opacity", function(window)
 	local overrides = window:get_config_overrides() or {}
 	if not overrides.window_background_opacity then
 		overrides.window_background_opacity = background_opacity
@@ -240,6 +265,16 @@ wezterm.on("toggle-opacity", function(window, pane)
 		overrides.window_background_opacity = nil
 	end
 	window:set_config_overrides(overrides)
+	return false
+end)
+
+-- cause the gui to match the rename behavior of ctrl+alt+t
+wezterm.on("new-tab-button-click", function(window, pane, button, default_action)
+	if default_action then
+		window:perform_action(default_action, pane)
+		window:active_tab():set_title("-")
+	end
+	return false
 end)
 
 -- use hyperlinks in terminal, ls navigation, open files
@@ -297,7 +332,7 @@ wezterm.on("open-uri", function(window, pane, uri)
 	-- without a return value, we allow default actions
 end)
 
-wezterm.on("update-right-status", function(window, pane)
+wezterm.on("update-right-status", function(window)
 	window:set_right_status(window:active_workspace())
 end)
 
