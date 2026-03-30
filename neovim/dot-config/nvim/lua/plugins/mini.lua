f3bb01d7d04b942f0d48ab41a0427820536ddf2f
Alexander Gertsch <agertsch11@gmail.com>
2026-03-30 21:52:48 +0200

    fix(neovim): update mini-nvim urls, remove keybind shadowing
    the urls changed a while ago, with forwarding but changing it is still
    good
    
    with nvim 0.12 the textobject incremental binds an and in are there,
    mini-ai shadows those which I don't want

--- neovim/dot-config/nvim/lua/plugins/mini.lua
+++ neovim/dot-config/nvim/lua/plugins/mini.lua
@@ -2,10 +2,10 @@
 
 local M = {
   spec = function(spec)
-    Add_plugin(spec, 'echasnovski/mini.ai', {
+    Add_plugin(spec, 'nvim-mini/mini.ai', {
       version = 'stable',
     })
-    Add_plugin(spec, 'echasnovski/mini.surround', {
+    Add_plugin(spec, 'nvim-mini/mini.surround', {
       version = 'stable',
     })
     -- mini.icons is setup in base.lua
@@ -15,36 +15,42 @@ local M = {
 function M.config()
   -- Extend and create a/i textobjects
   -- mainly use b (brace), q (quotes), f (function) and sometimes ? is good
-  require('mini.ai').setup({
-    mappings = {
-      around = 'a',
-      inside = 'i',
-      -- NOTE: These override built-in LSP selection mappings on Neovim>=0.12
-      -- Map LSP selection manually to use it (see `:h MiniAi.config`)
-      around_next = 'an',
-      inside_next = 'in',
-      around_last = 'al',
-      inside_last = 'il',
-      -- Move cursor to corresponding edge of `a` textobject
-      goto_left = 'g[',
-      goto_right = 'g]',
-    },
-  })
+  local mai = P_require('mini.ai', true)
+  if mai then
+    mai.setup({
+      mappings = {
+        around = 'a',
+        inside = 'i',
+        -- Disable these to keep the built-in LSP selection mappings on Neovim>=0.12
+        -- an and in in visual mode
+        around_next = '',
+        inside_next = '',
+        around_last = '',
+        inside_last = '',
+        -- Move cursor to corresponding edge of `a` textobject
+        goto_left = 'g[',
+        goto_right = 'g]',
+      },
+    })
+  end
 
   -- modify surrounding objects like quotes
-  require('mini.surround').setup({
-    mappings = {
-      add = 'sa', -- Add surrounding in Normal and Visual modes
-      delete = 'sd', -- Delete surrounding
-      find = 'sf', -- Find surrounding (to the right)
-      find_left = 'sF', -- Find surrounding (to the left)
-      highlight = 'sh', -- Highlight surrounding
-      replace = 'sr', -- Replace surrounding
-      update_n_lines = 'sn', -- Update `n_lines`
-      suffix_last = 'l', -- Suffix to search with "prev" method
-      suffix_next = 'n', -- Suffix to search with "next" method
-    },
-  })
+  local msr = P_require('mini.surround', true)
+  if msr then
+    msr.setup({
+      mappings = {
+        add = 'sa', -- Add surrounding in Normal and Visual modes
+        delete = 'sd', -- Delete surrounding
+        find = 'sf', -- Find surrounding (to the right)
+        find_left = 'sF', -- Find surrounding (to the left)
+        highlight = 'sh', -- Highlight surrounding
+        replace = 'sr', -- Replace surrounding
+        update_n_lines = 'sn', -- Update `n_lines`
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
+      },
+    })
+  end
 end
 
 return M
